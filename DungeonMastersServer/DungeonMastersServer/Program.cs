07f66e2e1366478943139b6461497f32245a284c
slidrex <superpes3000@gmail.com>
2025-02-28 00:35:08 +0500

    init projects

---
+++ DungeonMastersServer/DungeonMastersServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DungeonMastersServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
