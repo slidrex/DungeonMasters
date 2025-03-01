b5e69d8acddea5997962761446fbed4f2757a714
richismyhoo <richismyhoo@mail.ru>
2025-03-01 15:56:36 +0300

    refactor: chatMessages

--- DungeonMastersServer/DungeonMastersServer/MessageHandlers/ChatMessageHandler.cs
+++ DungeonMastersServer/DungeonMastersServer/MessageHandlers/ChatMessageHandler.cs
@@ -1,33 +1,17 @@
-﻿using Riptide;
+﻿using DungeonMastersServer.Services;
+using Riptide;
 
 namespace DungeonMastersServer.MessageHandlers;
 
 public class ChatMessageHandler
 {
-    /*
+
     [MessageHandler((ushort)ClientToServerId.sendChatMessage)]
     private static void HandleChatMessagePackage(ushort fromClient, Message message)
     {
-        Singleton.HandleChatMessage(fromClient, message);
-    }
-
-    public void HandleChatMessage(ushort fromClient, Message message)
-    {
-        var username = GetUsername(fromClient);
         var chatMessage = message.GetString();
         
-        Console.WriteLine($"{username}: {chatMessage}");
-        
-        var msg = Message.Create(MessageSendMode.Reliable, (ushort)ServerToClientId.playerChatMessage);
-        msg.AddString(username);
-        msg.AddString(chatMessage);
-        
-        NetworkManager.Server.SendToAll(msg);
+        ChatMessageService.Service.SendChatMessage(fromClient, chatMessage);
     }
 
-    private static string GetUsername(ushort playerId)
-    {
-        return PlayerMessageHandler.Players[playerId].Username;
-    }
-    */
 }
---
+++ DungeonMastersServer/DungeonMastersServer/Services/ChatMessageService.cs
@@ -0,0 +1,20 @@
+﻿using DungeonMastersServer.MessageHandlers;
+using DungeonMastersServer.Repositories;
+using Riptide;
+
+namespace DungeonMastersServer.Services;
+
+public class ChatMessageService : SingletonService<ChatMessageService>
+{
+    public void SendChatMessage(ushort fromClient, string chatMessage)
+    {
+        var player = ClientRepository.Service.GetPlayer(fromClient);
+        var playerNickname = player.Username;
+
+        var msg = Message.Create(MessageSendMode.Reliable, (ushort)ServerToClientId.playerChatMessage);
+        msg.AddString(playerNickname);
+        msg.AddString(chatMessage);
+        
+        NetworkManager.Server.SendToAll(msg);
+    }
+}
